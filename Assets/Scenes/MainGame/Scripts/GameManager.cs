1f758b7cd254154c6d9d04bf9d983f99ed6f6939
Gaagaa0719 <cwksp000@gmail.com>
2026-08-27 21:01:32 +0900

    wip: ゲーム進行大改修中

--- Assets/Scenes/MainGame/Scripts/GameManager.cs
+++ Assets/Scenes/MainGame/Scripts/GameManager.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Game.Result;
-using System.Threading.Tasks;
 
 public enum GamePhase
 {
@@ -12,158 +9,50 @@ public enum GamePhase
     Battle
 }
 
-public class GameManager : MonoBehaviour
+namespace Sakemottekoi.Maingame
 {
-    private static GameManager instance;
-    public static GameManager GetInstance() { return instance; }
-
-    public GamePhase currentPhase;
-
-    [Header("BGMのオーディオソース")]
-    public AudioSource bgmSource;
-
-    [Header("SEのオーディオソース")]
-    public AudioSource seSource;
-
-    [Header("アイテム選択用UI")]
-    [SerializeField]
-    private ItemOptions options;
-
-    [Header("敵キャラクタ")]
-    [SerializeField]
-    private Enemy enemy;
-
-    [Header("プレイヤーキャラクタ")]
-    [SerializeField]
-    private Player player;
-
-    [Header("勝利時の背景画像")]
-    [SerializeField]
-    private Sprite WinBackground;
-
-    [Header("敗北時の背景画像")]
-    [SerializeField]
-    private Sprite LoseBackground;
-
-    private bool isPlaying = false;
-    private bool waiting = false;
-    private GlassManager glassManager;
-    private MiniGameManager miniGameManager;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        instance = this;
-    }
+        public static GameManager Instance { private set; get; }
 
-    private void Start()
-    {
-        miniGameManager = MiniGameManager.instance;
-        glassManager = GlassManager.instance;
-        StartCoroutine(StartGameLoop());
-    }
+        public GamePhase CurrentPhase { private set; get; }
 
-    private IEnumerator StartGameLoop()
-    {
-        isPlaying = true;
-        while (isPlaying)
+        [Header("BGMのオーディオソース")]
+        private readonly AudioSource bgmSource;
+
+        [Header("SEのオーディオソース")]
+        private readonly AudioSource seSource;
+
+        private bool isPlaying = false;
+
+        private void Awake()
         {
-            // グラス初期化
-            glassManager.Init();
-
-            // アイテム選択フェーズ開始
-            currentPhase = GamePhase.ItemSelection;
-            waiting = true;
-            options.StartItemSelection();
-            yield return new WaitWhile(() => waiting);
-            StartCoroutine(options.EndItemSelection());
-
-            // 戦闘準備フェーズ開始
-            currentPhase = GamePhase.Prepare;
-            waiting = true;
-            yield return new WaitWhile(() => waiting);
-
-            // 戦闘フェーズ開始。
-            currentPhase = GamePhase.Battle;
-            yield return StartCoroutine(nameof(Battle));
-
-            if(enemy.GetDegreePoint() >= enemy.maxDegreePoint)
-            {
-                var data = new ResultSceneData
-                {
-                    ResultType = ResultType.Win,
-                    CharacterName = "リリス",
-                    WinBackgroundImage = WinBackground,
-                    FinalScore = 1000,
-                    Rank = ResultRank.S
-                };
-                ResultSceneLoader.SetData(data);
-                SceneManager.LoadScene("ResultScene");
-                yield break;
-            }
-
-            // ミニゲームフェーズ開始
-            currentPhase = GamePhase.Minigame;
-            MiniGame miniGame = miniGameManager.GetRandomOne();
-
-            // ミニゲームを実行
-            Task<bool> task = miniGame.StartGameAsync(player.GetDegreePoint());
-
-            // ミニゲームが終了するまで待機
-            yield return new WaitUntil(() => task.IsCompleted);
-
-            // 結果を受け取る
-            bool isSuccessed = task.Result;
-            if (!isSuccessed && player.GetDegreePoint() >= player.maxDegreePoint)
-            {
-                var data = new ResultSceneData
-                {
-                    ResultType = ResultType.Lose, // または ResultType.Lose
-                    CharacterName = "リリス",
-                    WinBackgroundImage = LoseBackground,  // 勝利時のみ使用。キャラごとに異なる酔いつぶれ画像
-                    FinalScore = 1000,
-                    Rank = ResultRank.S                   // ResultRank.E〜S。ランク算出ロジックは呼び出し側の責務
-                };
-
-                ResultSceneLoader.SetData(data);
-                SceneManager.LoadScene("ResultScene");
-            }
-
-            if (isSuccessed) player.AddDegreePoint(-5);
-            else player.AddDegreePoint(20);
-            miniGame.gameObject.SetActive(false);
+            Instance = this;
         }
-    }
 
-    // 戦闘処理
-    private IEnumerator Battle()
-    {
-        currentPhase = GamePhase.Battle;
+        private void Start()
+        {
+            StartCoroutine(StartGameLoop());
+        }
 
-        // 互いのグラスを交換
-        yield return StartCoroutine(glassManager.SwapGlass());
+        private IEnumerator StartGameLoop()
+        {
+            isPlaying = true;
+            while (isPlaying)
+            {
+                AlcholStockManager.Instance.ReplenishStock();
+                yield return null;
+            }
+        }
 
-        Glass playerGlass = glassManager.PlayerGlass.GetComponent<Glass>();
-        Glass enemyGlass = glassManager.EnemyGlass.GetComponent<Glass>();
+        static public AudioSource GetBGMSource()
+        {
+            return Instance.bgmSource;
+        }
 
-        player.AddDegreePoint(enemyGlass.CalcDegreePoint());
-        yield return new WaitForSeconds(1.0f);
-
-        enemy.AddDegreePoint(playerGlass.CalcDegreePoint());
-        yield return new WaitForSeconds(1.0f);
-    }
-
-    public void SetWainting (bool value)
-    {
-        waiting = value;
-    }
-
-    static public AudioSource GetBGMSource()
-    {
-        return instance.bgmSource;
-    }
-
-    static public AudioSource GetSESource()
-    {
-        return instance.seSource;
+        static public AudioSource GetSESource()
+        {
+            return Instance.seSource;
+        }
     }
 }
