fd740a310dac26d6d0cc33962d29c11ae0741a76
Gaagaa0719 <cwksp000@gmail.com>
2026-09-13 23:47:36 +0900

    refactor: フェーズマネージャーの共通処理を抜き出し基底クラスを作成

---
+++ Assets/Scenes/MainGame/Scripts/Managers/PhaseManagers/ItemSelectionPhaseManager.cs
@@ -0,0 +1,29 @@
+﻿namespace Sakemottekoi.MainGame
+{
+    public class ItemSelectionPhaseManager: PhaseManager
+    {
+        public static ItemSelectionPhaseManager Instance;
+        protected override GamePhase TargetPhase => GamePhase.ItemSelection;
+
+        // フェーズ中に選択可能なアイテム最大数
+        public static int SelectableItemCount { private set; get; } = 4;
+        // フェーズ中に選択したアイテムの数
+        public static int SelectedItemCount { private set; get; } = 0;
+
+        protected override void Awake()
+        {
+            Instance = this;
+            base.Awake();
+        }
+
+        public static void AddSelectedItemCount()
+        {
+            SelectedItemCount++;
+        }
+
+        protected override void StartPhase()
+        {
+            SelectedItemCount = 0;
+        }
+    }
+}
---
+++ Assets/Scenes/MainGame/Scripts/Managers/PhaseManagers/PhaseManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Sakemottekoi.MainGame
+{
+    public abstract class PhaseManager:  MonoBehaviour
+    {
+        public bool IsFinished { protected set; get; } = false;
+        protected abstract GamePhase TargetPhase { get; }
+
+        protected virtual void Awake()
+        {
+            GameManager.OnPhaseChanged += InternalStartPhase;
+            Init();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            GameManager.OnPhaseChanged -= InternalStartPhase;
+        }
+
+        // 初期化処理
+        protected virtual void Init() { }
+
+        // フェーズ開始処理
+        private void InternalStartPhase(GamePhase phase) {
+            if (phase != TargetPhase) return;
+            IsFinished = false;
+            StartPhase();
+        }
+
+        // 処理追加用
+        protected virtual void StartPhase() { }
+
+        // フェーズ終了処理
+        public virtual void EndPhase() {
+            IsFinished = true;
+        }
+    }
+}
