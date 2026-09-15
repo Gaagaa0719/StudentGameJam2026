495d9897a1e420055b0c9dc9a8e0f404400cf4b2
Gaagaa0719 <cwksp000@gmail.com>
2026-09-15 15:19:29 +0900

    add: 試験的に紙幣に説明テキストを追加

--- Assets/Scenes/MainGame/Scripts/Item/PaperMoney.cs
+++ Assets/Scenes/MainGame/Scripts/Item/PaperMoney.cs
@@ -8,7 +8,7 @@ namespace Sakemottekoi.MainGame
 
         public override string SimpleDescription => "提示されている中から酒を一つ選び、選んだ酒のアルコール度数がわかる";
 
-        public override string Description => "";
+        public override string Description => "提示された3つのお酒から一つ選び、選んだお酒に一度浸して取り出す。取り出した紙幣に火を点け燃え具合でそのお酒の酔い度がわかる。\n小は燃えず。中は少し、大はとても燃える。";
 
         protected override IEnumerator InternalUse()
         {
