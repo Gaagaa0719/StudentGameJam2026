8a66753b4fef670b346758a57688f7279a3caa7e
Gaagaa0719 <cwksp000@gmail.com>
2026-09-15 21:21:24 +0900

    add: フェーズごとに目的を表示できるスクリプトを追加

---
+++ Assets/Scenes/MainGame/Scripts/UI/ObjectiveUI.cs
@@ -0,0 +1,53 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Sakemottekoi.MainGame
+{
+    [Serializable]
+    public class PhaseObjectiveData
+    {
+        public GamePhase Phase;
+        public string ObjectiveText;
+    }
+
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    class ObjectiveUI : FadeUIBase
+    {
+        [Header("表示時間")]
+        [SerializeField]
+        private float stayTime;
+
+        [Header("各フェーズの目的テキスト")]
+        [SerializeField]
+        private List<PhaseObjectiveData> phaseObjectives = new();
+
+        private TextMeshProUGUI textComp;
+        private Coroutine coroutine;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            textComp = GetComponent<TextMeshProUGUI>();
+            GameManager.OnPhaseChanged += (phase) => {
+                PhaseObjectiveData objectiveData = phaseObjectives.Find(v => v.Phase == phase);
+                if (objectiveData == null) return;
+                if (coroutine != null) return;
+                coroutine = StartCoroutine(ShowObjective(objectiveData));
+            };
+        }
+
+        private IEnumerator ShowObjective(PhaseObjectiveData data)
+        {
+            textComp.text = data.ObjectiveText;
+            FadeIn();
+            yield return new WaitForSeconds(stayTime);
+            FadeOut();
+            coroutine = null;
+        }
+    }
+}
