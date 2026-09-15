8f54cd976170422191cde6853a92b0363c5a7337
Gaagaa0719 <cwksp000@gmail.com>
2026-09-15 15:07:47 +0900

    add: アイテム使用フォームを追加

---
+++ Assets/Scenes/MainGame/Scripts/UI/ItemUseForm.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+namespace Sakemottekoi.MainGame
+{
+    public class ItemUseForm : FadeUIBase
+    {
+        [SerializeField] Image ItemIcon;
+        [SerializeField] TextMeshProUGUI ItemName;
+        [SerializeField] TextMeshProUGUI ItemDescription;
+
+        [SerializeField] Button CloseButton;
+        [SerializeField] Button UseButton;
+
+        private Item showingItem;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Item.OnClicked += ShowForm;
+            CloseButton.onClick.AddListener(() => StartCoroutine(Hide()));
+            UseButton.onClick.AddListener(() => { if (showingItem != null) StartCoroutine(showingItem.Use()); });
+        }
+
+        private void ShowForm(Item item)
+        {
+            if (GameManager.Instance.CurrentPhase != GamePhase.ItemUse) return;
+            ItemIcon.sprite = item.ItemImage;
+            ItemName.text = item.DisplayName;
+            ItemDescription.text = item.Description;
+
+            showingItem = item;
+
+            StartCoroutine(Show());
+        }
+    }
+}
